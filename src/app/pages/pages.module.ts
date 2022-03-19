d93db24383f443b3663f5a6603ac59dc6b575708
TomasDep <tomas.valenzuelacb@gmail.com>
2022-03-19 16:01:53 -0300

    [CHG] se modifico modulo añadiendo FormModule y logica de llamada de componentes

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { ComponentsModule } from '../components/components.module';
+import { SharedModule } from '../shared/shared.module';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
-import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -17,9 +19,11 @@ import { SharedModule } from '../shared/shared.module';
     PagesComponent
   ],
   imports: [
+    FormsModule,
     CommonModule,
     SharedModule,
-    RouterModule
+    RouterModule,
+    ComponentsModule
   ],
   exports: [
     DashboardComponent,
