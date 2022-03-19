c8ce91ec503593e11d094003468d56b258f6844e
TomasDep <tomas.valenzuelacb@gmail.com>
2022-03-19 18:14:01 -0300

    [ADD] se agrego funcion de script para inicicalizacion de la informacion

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { SettingsService } from '../services/settings.service';
+
+declare function customInitfunction(): any;
 
 @Component({
   selector: 'app-pages',
@@ -8,9 +11,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PagesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private settingsService: SettingsService) { }
 
   ngOnInit(): void {
+    customInitfunction();
   }
 
 }
