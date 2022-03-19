1ab5f736641bec54e1368792522554a9f4c9f774
TomasDep <tomas.valenzuelacb@gmail.com>
2022-03-19 16:02:18 -0300

    [ADD] se termino de crear la pagina de grafica1

--- src/app/pages/grafica1/grafica1.component.ts
+++ src/app/pages/grafica1/grafica1.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ChartData } from 'chart.js';
 
 @Component({
   selector: 'app-grafica1',
@@ -6,11 +7,59 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class Grafica1Component implements OnInit {
+export class Grafica1Component {
+  public titulo1: string = 'Finanzas';
+  public titulo2: string = 'Marketing';
+  public titulo3: string = 'Seguridad';
+  public titulo4: string = 'Atenciones';
 
-  constructor() { }
+  public label1: string[] = ['Ingresos', 'Perdidas', 'Deudas'];
+  public label2: string[] = ['Likes', 'Dislikes', 'Sin clasificar'];
+  public label3: string[] = ['Vulnerabilidades', 'Hackeos', 'Soluciones'];
+  public label4: string[] = ['Enero', 'Febrero', 'Marzo'];
 
-  ngOnInit(): void {
-  }
+  public data1:number[] = [5800000, 3000000, 100000];
+  public data2:number[] = [1000580, 120560, 1002];
+  public data3:number[] = [150, 3, 120];
+  public data4:number[] = [150000, 120000, 175000];
 
+  public grafica1: ChartData<'doughnut'> = {
+    labels: this.label1,
+    datasets: [
+      { 
+        data: this.data1,
+        backgroundColor: ['#6857e6', '#009fee', '#f02059']
+      },
+    ],
+  };
+
+  public grafica2: ChartData<'doughnut'> = {
+    labels: this.label2,
+    datasets: [
+      { 
+        data: this.data2,
+        backgroundColor: ['#6857e6', '#009fee', '#f02059']
+      },
+    ],
+  };
+
+  public grafica3: ChartData<'doughnut'> = {
+    labels: this.label3,
+    datasets: [
+      { 
+        data: this.data3,
+        backgroundColor: ['#6857e6', '#009fee', '#f02059']
+      },
+    ],
+  };
+  
+  public grafica4: ChartData<'doughnut'> = {
+    labels: this.label4,
+    datasets: [
+      { 
+        data: this.data4,
+        backgroundColor: ['#6857e6', '#009fee', '#f02059']
+      },
+    ],
+  };
 }
