cd14bc82cb978ce6ef4cb8e35a24fde7e6e41480
TomasDep <tomas.valenzuelacb@gmail.com>
2022-03-19 18:12:22 -0300

    [ADD] se agrego los modules de ruta y formulario

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
@@ -10,7 +12,9 @@ import { LoginComponent } from './login/login.component';
     RegisterComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    FormsModule
   ],
   exports: [
     LoginComponent,
