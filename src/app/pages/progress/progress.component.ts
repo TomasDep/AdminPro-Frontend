cac7f770fbb46bcad67a6c172a91b16f9d25533d
TomasDep <tomas.valenzuelacb@gmail.com>
2022-03-19 16:02:36 -0300

    [ADD] se termino de crear la pagina de progress

--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
-  styles: [
-  ]
+  styleUrls: ['./progress.component.css']
 })
-export class ProgressComponent implements OnInit {
+export class ProgressComponent {
 
-  constructor() { }
+  public progreso1: number = 15;
+  public progreso2: number = 25;
 
-  ngOnInit(): void {
+  get getProgreso1 () {
+    return `${this.progreso1}%`
   }
 
+  get getProgreso2 () {
+    return `${this.progreso2}%`
+  }
+
+  
 }
