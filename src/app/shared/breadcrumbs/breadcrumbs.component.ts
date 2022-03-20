f6cd7bf1926b017091e00a5887c7c42ce590a4af
TomasDep <tomas.valenzuelacb@gmail.com>
2022-03-20 17:20:13 -0300

    [CHG] se cambiaron los titulos de forma dinamica

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { ActivationEnd, Router } from '@angular/router';
+import { filter, map, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-breadcrumbs',
   templateUrl: './breadcrumbs.component.html',
   styleUrls: ['./breadcrumbs.component.css']
 })
-export class BreadcrumbsComponent implements OnInit {
+export class BreadcrumbsComponent implements OnDestroy{
 
-  constructor() { }
+  public titulo: string = '';
+  public tituloSub$: Subscription = new Subscription();
 
-  ngOnInit(): void {
+  constructor(private router: Router) {
+    this.tituloSub$ = this.getDataRutas()
+                        .subscribe(({titulo}) => {
+                          this.titulo = titulo;
+                          document.title = `AdminPro - ${ this.titulo }`;
+                        });
   }
 
+  ngOnDestroy(): void {
+    this.tituloSub$.unsubscribe();
+  }
+
+  getDataRutas() {
+    return this.router.events
+      .pipe(
+        filter((event): event is ActivationEnd => event instanceof ActivationEnd),
+        filter((event: ActivationEnd) => event.snapshot.firstChild === null),
+        map((event: ActivationEnd) => event.snapshot.data),
+      )
+  }
 }
