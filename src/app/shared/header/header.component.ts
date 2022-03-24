6372ed075291cfa806817daf6ca7a7f54614c87d
TomasDep <tomas.valenzuelacb@gmail.com>
2022-03-24 20:04:36 -0300

    [ADD] se agrego una clase para resolver el problema de pointer

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
-  constructor() { }
+  constructor(private usuarioService: UsuarioService) { }
 
-  ngOnInit(): void {
+  logout() {
+    this.usuarioService.logout();
   }
-
 }
