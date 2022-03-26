7a25c767ee96547a3d62760be08adc58bc30f531
TomasDep <tomas.valenzuelacb@gmail.com>
2022-03-26 20:38:35 -0300

    [ADD] ruta usuarios y seccion de menu de mantenedores

--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -11,6 +11,7 @@ import { AccountSettingsComponent } from './account-settings/account-settings.co
 import { PromesasComponent } from './promesas/promesas.component';
 import { RxjsComponent } from './rxjs/rxjs.component';
 import { PerfilComponent } from './perfil/perfil.component';
+import { UsuariosComponent } from './mantenimientos/usuarios/usuarios.component';
 
 const routes: Routes = [
   { 
@@ -25,6 +26,8 @@ const routes: Routes = [
       { path: 'promesas', component: PromesasComponent, data: { titulo: 'Promesas' } },
       { path: 'rxjs', component: RxjsComponent, data: { titulo: 'RXJS' } },
       { path: 'perfil', component: PerfilComponent, data: { titulo: 'Perfil de usuario' } },
+      // Mantenimientos
+      { path: 'usuarios', component: UsuariosComponent, data: { titulo: 'Usuarios de la aplicación' } },
     ]
   },
 ];
