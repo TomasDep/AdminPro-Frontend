2c7790cbaeaaa2d58d22aa5ea3730eecf4eec406
TomasDep <tomas.valenzuelacb@gmail.com>
2022-03-30 19:47:47 -0300

    [ADD] se crean busquedas para medicos y hospitales

--- src/app/services/busqueda.service.ts
+++ src/app/services/busqueda.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
+
 import { Usuario } from '../models/usuario.model';
+import { Hospital } from '../models/hospital.model';
+import { Medico } from '../models/medico.model';
 
 const baseUrl = environment.baseUrl;
 
@@ -17,7 +21,7 @@ export class BusquedaService {
     return sessionStorage.getItem('token') || '';
   }
 
-  get headers() {
+  get headers(): any {
     return { headers: { 'x-token': this.token } };
   }
 
@@ -27,16 +31,51 @@ export class BusquedaService {
     );
   }
 
-  buscar(tipo: 'usuarios'|'medicos'|'hospitales', termino: string) {
+  private transformarHospitales(resultados: any[]): Hospital[] {
+    return resultados;
+  }
+
+  private transformarMedicos(resultados: any[]): Medico[] {
+    return resultados;
+  }
+
+  buscarUsuarios(tipo: string, termino: string): Observable<Usuario[]> {
     const url = `${ baseUrl }/todo/coleccion/${ tipo }/${ termino }`;
-    return this.http.get<any []>(url, this.headers)
+    return this.http.get<any[]>(url, this.headers)
                 .pipe(
                   map((resp: any) => {
-                    switch (tipo) {
-                      case 'usuarios':
-                        return this.transformarUsuarios(resp.resultados);                    
-                      default:
-                        return [];
+                    if (tipo.includes('usuarios')) {
+                      return this.transformarUsuarios(resp.resultados);
+                    } else {
+                      return [];
+                    }
+                  })
+                );
+  }
+
+  buscarHospitales(tipo: string, termino: string): Observable<Hospital[]> {
+    const url = `${ baseUrl }/todo/coleccion/${ tipo }/${ termino }`;
+    return this.http.get<any[]>(url, this.headers)
+                .pipe(
+                  map((resp: any) => {
+                    if (tipo.includes('hospitales')) {
+                      return this.transformarHospitales(resp.resultados);
+                    } else {
+                      return [];
+                    }
+                  })
+                );
+  }
+
+  buscarMedicos(tipo: string, termino: string): Observable<Medico[]> {
+    const url = `${ baseUrl }/todo/coleccion/${ tipo }/${ termino }`;
+    return this.http.get<any[]>(url, this.headers)
+                .pipe(
+                  map((resp: any) => {
+                    if (tipo.includes('medicos')) {
+                      return this.transformarMedicos(resp.resultados);
+                    } else {
+                      return [];
                     }
                   })
                 );
