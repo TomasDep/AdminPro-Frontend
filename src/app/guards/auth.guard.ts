4f0e224235d07afc14daee4b8fcd557d6cb57eb4
TomasDep <tomas.valenzuelacb@gmail.com>
2022-04-06 16:24:23 -0400

    [CHG] se implementa lazyload

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
-import { tap } from 'rxjs';
+import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, Router, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
+import { Observable, tap } from 'rxjs';
 
 import { UsuarioService } from '../services/usuario.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
 
   constructor(private usuarioService: UsuarioService, private router: Router) { }
 
@@ -21,4 +21,13 @@ export class AuthGuard implements CanActivate {
     );
   }
   
+  canLoad(route: Route, segments: UrlSegment[]): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    return this.usuarioService.validarToken().pipe(
+      tap(estaAutenticado => {
+        if (!estaAutenticado) {
+          this.router.navigateByUrl('/login');
+        } 
+      })
+    );
+  }
 }
--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -3,47 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AuthGuard } from '../guards/auth.guard';
 
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
-import { ProgressComponent } from './progress/progress.component';
-import { AccountSettingsComponent } from './account-settings/account-settings.component';
-import { PromesasComponent } from './promesas/promesas.component';
-import { RxjsComponent } from './rxjs/rxjs.component';
-import { PerfilComponent } from './perfil/perfil.component';
-import { UsuariosComponent } from './mantenimientos/usuarios/usuarios.component';
-import { HospitalesComponent } from './mantenimientos/hospitales/hospitales.component';
-import { MedicosComponent } from './mantenimientos/medicos/medicos.component';
-import { MedicoComponent } from './mantenimientos/medicos/medico.component';
-import { BusquedaComponent } from './busqueda/busqueda.component';
-import { AdminGuard } from '../guards/admin.guard';
 
 const routes: Routes = [
   { 
     path: 'dashboard', 
     component: PagesComponent,
     canActivate: [AuthGuard],
-    children: [
-      { path: '', component: DashboardComponent, data: { titulo: 'Dashboard' } },
-      { path: 'account-settings', component: AccountSettingsComponent, data: { titulo: 'Account Settings' } },
-      { path: 'busqueda/:termino', component: BusquedaComponent, data: { titulo: 'Coincidencias encontradas...' } },
-      { path: 'grafica1', component: Grafica1Component, data: { titulo: 'Grafica #1' } },
-      { path: 'progress', component: ProgressComponent, data: { titulo: 'Progress' } },
-      { path: 'promesas', component: PromesasComponent, data: { titulo: 'Promesas' } },
-      { path: 'rxjs', component: RxjsComponent, data: { titulo: 'RXJS' } },
-      { path: 'perfil', component: PerfilComponent, data: { titulo: 'Perfil de usuario' } },
-      // Mantenimientos
-      { path: 'hospitales', component: HospitalesComponent, data: { titulo: 'Mantenimiento de hospitales' } },
-      { path: 'medicos', component: MedicosComponent, data: { titulo: 'Mantenimiento de medicos' } },
-      { path: 'medico/:id', component: MedicoComponent, data: { titulo: 'Mantenimiento de medicos' } },
-      // Rutas Admin
-      { 
-        path: 'usuarios', 
-        canActivate: [AdminGuard], 
-        component: UsuariosComponent, 
-        data: { titulo: 'Mantenimiento de usuarios' } 
-      },
-    ]
+    canLoad: [AuthGuard],
+    loadChildren: () => import('./child-routes.module').then(m => m.ChildRoutesModule)
   },
 ];
 
