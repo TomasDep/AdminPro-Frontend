14726855002b341a78c7a20d399d5c573aab323c
TomasDep <tomas.valenzuelacb@gmail.com>
2022-04-13 18:53:26 -0400

    [CHG] se arregla error que no tomaba tema del usuario al loguearse

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { SidebarService } from '@services/index';
+import { SettingsService } from '@services/settings.service';
 
 declare function customInitfunction(): any;
 
@@ -11,14 +12,17 @@ declare function customInitfunction(): any;
   ]
 })
 export class PagesComponent implements OnInit {
+  public currentDate: Date = new Date();
 
   constructor(
-    private sidebarService: SidebarService
+    private sidebarService: SidebarService,
+    private settingsService: SettingsService
   ) { }
 
   ngOnInit(): void {
     customInitfunction();
     this.sidebarService.cargarMenu();
+    this.settingsService.checkCurrentTheme();
   }
 
 }
